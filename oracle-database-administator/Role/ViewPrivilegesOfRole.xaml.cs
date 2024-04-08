f467e2dbd8cd42cb54b67eb5bc55d8f46a991974
Phan Quoc Huy <21120082@student.hcmus.edu.vn>
2024-04-09 05:03:42 +0700

    commit images setup

--- oracle-database-administator/Role/ViewPrivilegesOfRole.xaml.cs
+++ oracle-database-administator/Role/ViewPrivilegesOfRole.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,127 @@ namespace oracle_database_administator.Role
     /// </summary>
     public partial class ViewPrivilegesOfRole : Page
     {
+        OracleConnection conn;
         public ViewPrivilegesOfRole()
         {
             InitializeComponent();
+        }
+
+        private void UpdateUserGrid()
+        {
+            try
+            {
+                string query = "SELECT ROLE, ROLE_ID FROM SYS.DBA_ROLES";
+                using (OracleCommand command = new OracleCommand(query, conn))
+                {
+                    using (OracleDataAdapter adapter = new OracleDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        RoleDataGrid.ItemsSource = dataTable.DefaultView;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow is MainWindow mainWindow && mainWindow.MainFrame != null)
+            {
+                mainWindow.MainFrame.Navigate(new oracle_database_administator.Dashboard());
+            }
+        }
+
+        // Sử dụng sự kiện Unloaded để đảm bảo rằng kết nối được đóng khi chuyển khỏi Page
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            conn = Database.Instance.Connection;
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    Console.WriteLine("Connection opened successfully!");
+                    UpdateUserGrid();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to open connection.");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection error: " + ex.Message);
+            }
+        }
+
+        private void InsertButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string roleName = RoleNameTextBox.Text;
+
+                string query = @"BEGIN
+                            EXECUTE IMMEDIATE 'ALTER SESSION SET ""_ORACLE_SCRIPT"" = TRUE';
+                            EXECUTE IMMEDIATE 'CREATE ROLE " + roleName + "';" +
+                            " END;";
+
+
+                using (OracleCommand command = new OracleCommand(query, conn))
+                {
+                    int rowSelected = command.ExecuteNonQuery();
+
+                    if (rowSelected == -1)
+                    {
+                        MessageBox.Show("Create role successfully!");
+                        UpdateUserGrid();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cannot create role!");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Create error: " + ex.Message);
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow is MainWindow mainWindow && mainWindow.MainFrame != null)
+            {
+                mainWindow.MainFrame.Navigate(new oracle_database_administator.Dashboard());
+            }
+        }
+
+        private void RolesUserButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PriUserButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
