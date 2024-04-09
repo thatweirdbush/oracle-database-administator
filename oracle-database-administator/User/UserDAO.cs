5336d831a2932d969a2f137f1fb10b5d95c9b66d
Giangfs <123853667+Giangfs@users.noreply.github.com>
2024-04-09 20:45:32 +0700

    show privileges of user list ver1

---
+++ oracle-database-administator/User/UserDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oracle_database_administator.User
+{
+    internal class UserDAO
+    {
+    }
+    public class UserInfo
+    {
+        public string UserName { get; set; }
+
+        public UserInfo(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
