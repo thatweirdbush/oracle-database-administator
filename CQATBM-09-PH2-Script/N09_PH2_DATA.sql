08818a8df2efeeeb27e2cfb6cf5ce8316bac9632
Phan Quoc Huy <21120082@student.hcmus.edu.vn>
2024-06-01 05:11:19 +0700

    add: 3 SQL files from PH2

---
+++ CQATBM-09-PH2-Script/N09_PH2_DATA.sql
@@ -0,0 +1,186 @@
+-- Lớp: CQ2021/1
+-- Nhóm: 09
+-- Thành viên:
+-- 21120037 - Mã Thùy Anh
+-- 21120060 - Nguyễn Long Giang
+-- 21120082 - Phan Quốc Huy 
+-- 21120117 - Lê Thị Hồng Phượng
+
+
+
+----------------------------------------------------------------
+-- Chèn dữ liệu vào bảng N09_DONVI
+----------------------------------------------------------------
+INSERT INTO N09_DONVI (MADV, TENDV, TRGDV) VALUES ('DV001', 'Van phong khoa', 'NV003');
+INSERT INTO N09_DONVI (MADV, TENDV, TRGDV) VALUES ('DV002', 'Bo mon HTTT', 'NV002');
+INSERT INTO N09_DONVI (MADV, TENDV, TRGDV) VALUES ('DV003', 'Bo mon CNPM', 'NV004');
+INSERT INTO N09_DONVI (MADV, TENDV, TRGDV) VALUES ('DV004', 'Bo mon KHMT', 'NV005');
+INSERT INTO N09_DONVI (MADV, TENDV, TRGDV) VALUES ('DV005', 'Bo mon CNTT', 'NV006');
+INSERT INTO N09_DONVI (MADV, TENDV, TRGDV) VALUES ('DV006', 'Bo mon TGMT', 'NV007');
+INSERT INTO N09_DONVI (MADV, TENDV, TRGDV) VALUES ('DV007', 'Bo mon MMT va Vien thong', 'NV008');
+/
+
+----------------------------------------------------------------
+-- Chèn dữ liệu vào bảng N09_NHANSU
+----------------------------------------------------------------
+-- Thêm dữ liệu cho vai trò "Trưởng khoa"
+INSERT INTO N09_NHANSU (MANV, HOTEN, PHAI, NGSINH, PHUCAP, DT, VAITRO, MADV)
+VALUES ('NV001', 'Nguyễn Thị Minh Thư', 'Nu', TO_DATE('1991-01-01', 'YYYY-MM-DD'), 9000000, '0909123001', 'Truong khoa', 'DV001');
+
+
+----------------------------------------------------------------
+-- Thêm dữ liệu cho vai trò "Trưởng đơn vị"
+----------------------------------------------------------------
+DECLARE
+    v_manv CHAR(5);
+BEGIN
+    FOR i IN 1..6 LOOP
+        v_manv := 'NV1' || LPAD(i, 2, '0');        
+        INSERT INTO N09_NHANSU (MANV, HOTEN, PHAI, NGSINH, PHUCAP, DT, VAITRO, MADV)
+        VALUES (v_manv, 'Trưởng Đơn Vị ' || i, 
+        CASE WHEN MOD(i, 2) = 0 THEN 'Nam' ELSE 'Nu' END, 
+        TO_DATE('1970-01-01', 'YYYY-MM-DD'), 
+        1000000 + 10000 * i, 
+        '090912310' || MOD(i, 10), 
+        'Truong don vi', 
+        'DV00' || (MOD(i, 7) + 1));
+    END LOOP;
+END;
+/
+
+----------------------------------------------------------------
+-- Thêm dữ liệu cho vai trò "Giảng viên"
+----------------------------------------------------------------
+DECLARE
+    v_manv CHAR(5);
+BEGIN
+    FOR i IN 1..80 LOOP
+        v_manv := 'NV2' || LPAD(i, 2, '0');        
+        INSERT INTO N09_NHANSU (MANV, HOTEN, PHAI, NGSINH, PHUCAP, DT, VAITRO, MADV)
+        VALUES (v_manv, 'Giảng Viên ' || i, 
+        CASE WHEN MOD(i, 2) = 0 THEN 'Nam' ELSE 'Nu' END, 
+        TO_DATE('1990-01-01', 'YYYY-MM-DD'), 
+        4000000 + 30000 * i, 
+        '09091232' || MOD(i, 100), 
+        'Giang vien', 
+        'DV00' || (MOD(i, 7) + 1));
+    END LOOP;
+END;
+/
+
+----------------------------------------------------------------
+-- Thêm dữ liệu cho vai trò "Giáo vụ"
+----------------------------------------------------------------
+DECLARE
+    v_manv CHAR(5);
+BEGIN
+    FOR i IN 1..10 LOOP
+        v_manv := 'NV3' || LPAD(i, 2, '0');        
+        INSERT INTO N09_NHANSU (MANV, HOTEN, PHAI, NGSINH, PHUCAP, DT, VAITRO, MADV)
+        VALUES (v_manv, 'Giáo Vụ ' || i, 
+        CASE WHEN MOD(i, 2) = 0 THEN 'Nam' ELSE 'Nu' END, 
+        TO_DATE('1995-01-01', 'YYYY-MM-DD'), 
+        3000000 + 20000 * i, 
+        '090912330' || MOD(i, 10), 
+        'Giao vu', 
+        'DV00' || (MOD(i, 7) + 1));
+    END LOOP;
+END;
+/
+
+----------------------------------------------------------------
+-- Thêm dữ liệu cho vai trò "Nhân viên"
+----------------------------------------------------------------
+DECLARE
+    v_manv CHAR(5);
+BEGIN
+    FOR i IN 1..10 LOOP
+        v_manv := 'NV4' || LPAD(i, 2, '0');        
+        INSERT INTO N09_NHANSU (MANV, HOTEN, PHAI, NGSINH, PHUCAP, DT, VAITRO, MADV)
+        VALUES (v_manv, 'Nhân Viên ' || i, 
+        CASE WHEN MOD(i, 2) = 0 THEN 'Nam' ELSE 'Nu' END, 
+        TO_DATE('2000-01-01', 'YYYY-MM-DD'), 
+        2000000 + 20000 * i, 
+        '090912340' || MOD(i, 10), 
+        'Nhan vien', 
+        'DV00' || (MOD(i, 7) + 1));
+    END LOOP;
+END;
+/
+
+----------------------------------------------------------------
+-- Chèn dữ liệu vào bảng N09_SINHVIEN
+----------------------------------------------------------------
+INSERT INTO N09_SINHVIEN (MASV, HOTEN, PHAI, NGSINH, DIACHI, DT, MACT, MANGANH, SOTCTL, DTBTL) VALUES ('SV001', 'Nguyen Thi Lan', 'Nu', TO_DATE('2000-01-01', 'YYYY-MM-DD'), '123 Le Loi', '0911234567', 'CQ', 'CNPM', 100, 8.5);
+INSERT INTO N09_SINHVIEN (MASV, HOTEN, PHAI, NGSINH, DIACHI, DT, MACT, MANGANH, SOTCTL, DTBTL) VALUES ('SV002', 'Tran Van Minh', 'Nam', TO_DATE('2000-02-02', 'YYYY-MM-DD'), '456 Nguyen Hue', '0912345678', 'CQ', 'CNTT', 110, 7.8);
+INSERT INTO N09_SINHVIEN (MASV, HOTEN, PHAI, NGSINH, DIACHI, DT, MACT, MANGANH, SOTCTL, DTBTL) VALUES ('SV003', 'Le Thi Hoa', 'Nu', TO_DATE('2000-03-03', 'YYYY-MM-DD'), '789 Tran Phu', '0913456789', 'CLC', 'KHMT', 120, 9.0);
+INSERT INTO N09_SINHVIEN (MASV, HOTEN, PHAI, NGSINH, DIACHI, DT, MACT, MANGANH, SOTCTL, DTBTL) VALUES ('SV004', 'Pham Van Nam', 'Nam', TO_DATE('2000-04-04', 'YYYY-MM-DD'), '101 Le Thanh Ton', '0914567890', 'CTTT', 'TGMT', 95, 6.5);
+INSERT INTO N09_SINHVIEN (MASV, HOTEN, PHAI, NGSINH, DIACHI, DT, MACT, MANGANH, SOTCTL, DTBTL) VALUES ('SV005', 'Hoang Thi Thu', 'Nu', TO_DATE('2000-05-05', 'YYYY-MM-DD'), '102 Tran Hung Dao', '0915678901', 'VP', 'MMT', 105, 7.2);
+INSERT INTO N09_SINHVIEN (MASV, HOTEN, PHAI, NGSINH, DIACHI, DT, MACT, MANGANH, SOTCTL, DTBTL) VALUES ('SV006', 'Vu Van Lam', 'Nam', TO_DATE('2000-06-06', 'YYYY-MM-DD'), '103 Bach Dang', '0916789012', 'CQ', 'CNPM', 115, 8.0);
+INSERT INTO N09_SINHVIEN (MASV, HOTEN, PHAI, NGSINH, DIACHI, DT, MACT, MANGANH, SOTCTL, DTBTL) VALUES ('SV007', 'Bui Thi Tuyet', 'Nu', TO_DATE('2000-07-07', 'YYYY-MM-DD'), '104 Ngo Gia Tu', '0917890123', 'CLC', 'CNTT', 110, 8.4);
+INSERT INTO N09_SINHVIEN (MASV, HOTEN, PHAI, NGSINH, DIACHI, DT, MACT, MANGANH, SOTCTL, DTBTL) VALUES ('SV008', 'Do Van Hung', 'Nam', TO_DATE('2000-08-08', 'YYYY-MM-DD'), '105 Hai Ba Trung', '0918901234', 'CTTT', 'KHMT', 105, 6.9);
+INSERT INTO N09_SINHVIEN (MASV, HOTEN, PHAI, NGSINH, DIACHI, DT, MACT, MANGANH, SOTCTL, DTBTL) VALUES ('SV009', 'Ngo Thi Lan', 'Nu', TO_DATE('2000-09-09', 'YYYY-MM-DD'), '106 Le Dai Hanh', '0919012345', 'VP', 'TGMT', 100, 7.5);
+INSERT INTO N09_SINHVIEN (MASV, HOTEN, PHAI, NGSINH, DIACHI, DT, MACT, MANGANH, SOTCTL, DTBTL) VALUES ('SV010', 'Chu Van Tam', 'Nam', TO_DATE('2000-10-10', 'YYYY-MM-DD'), '107 Dien Bien Phu', '0910123456', 'CQ', 'MMT', 110, 8.1);
+/
+
+----------------------------------------------------------------
+-- Chèn dữ liệu vào bảng N09_HOCPHAN
+----------------------------------------------------------------
+INSERT INTO N09_HOCPHAN (MAHP, TENHP, SOTC, STLT, STTH, SOSVTD, MADV) VALUES ('HP001', 'Lap trinh C', 3, 30, 15, 50, 'DV001');
+INSERT INTO N09_HOCPHAN (MAHP, TENHP, SOTC, STLT, STTH, SOSVTD, MADV) VALUES ('HP002', 'Lap trinh Java', 4, 40, 20, 60, 'DV002');
+INSERT INTO N09_HOCPHAN (MAHP, TENHP, SOTC, STLT, STTH, SOSVTD, MADV) VALUES ('HP003', 'Cau truc du lieu', 3, 30, 15, 50, 'DV003');
+INSERT INTO N09_HOCPHAN (MAHP, TENHP, SOTC, STLT, STTH, SOSVTD, MADV) VALUES ('HP004', 'Co so du lieu', 3, 30, 15, 50, 'DV004');
+INSERT INTO N09_HOCPHAN (MAHP, TENHP, SOTC, STLT, STTH, SOSVTD, MADV) VALUES ('HP005', 'Mang may tinh', 4, 40, 20, 60, 'DV005');
+INSERT INTO N09_HOCPHAN (MAHP, TENHP, SOTC, STLT, STTH, SOSVTD, MADV) VALUES ('HP006', 'Tri tue nhan tao', 3, 30, 15, 50, 'DV006');
+INSERT INTO N09_HOCPHAN (MAHP, TENHP, SOTC, STLT, STTH, SOSVTD, MADV) VALUES ('HP007', 'Phan tich thiet ke HTTT', 4, 40, 20, 60, 'DV007');
+INSERT INTO N09_HOCPHAN (MAHP, TENHP, SOTC, STLT, STTH, SOSVTD, MADV) VALUES ('HP008', 'Khai thac du lieu', 3, 30, 15, 50, 'DV001');
+INSERT INTO N09_HOCPHAN (MAHP, TENHP, SOTC, STLT, STTH, SOSVTD, MADV) VALUES ('HP009', 'An toan thong tin', 3, 30, 15, 50, 'DV002');
+INSERT INTO N09_HOCPHAN (MAHP, TENHP, SOTC, STLT, STTH, SOSVTD, MADV) VALUES ('HP010', 'Lap trinh Python', 3, 30, 15, 50, 'DV003');
+/
+
+----------------------------------------------------------------
+-- Chèn dữ liệu vào bảng N09_KHMO
+----------------------------------------------------------------
+INSERT INTO N09_KHMO (MAHP, HK, NAM, MACT) VALUES ('HP001', 1, 2024, 'CQ');
+INSERT INTO N09_KHMO (MAHP, HK, NAM, MACT) VALUES ('HP002', 2, 2024, 'CLC');
+INSERT INTO N09_KHMO (MAHP, HK, NAM, MACT) VALUES ('HP003', 3, 2024, 'CTTT');
+INSERT INTO N09_KHMO (MAHP, HK, NAM, MACT) VALUES ('HP004', 1, 2024, 'VP');
+INSERT INTO N09_KHMO (MAHP, HK, NAM, MACT) VALUES ('HP005', 2, 2024, 'CQ');
+INSERT INTO N09_KHMO (MAHP, HK, NAM, MACT) VALUES ('HP006', 3, 2024, 'CLC');
+INSERT INTO N09_KHMO (MAHP, HK, NAM, MACT) VALUES ('HP007', 1, 2024, 'CTTT');
+INSERT INTO N09_KHMO (MAHP, HK, NAM, MACT) VALUES ('HP008', 2, 2024, 'VP');
+INSERT INTO N09_KHMO (MAHP, HK, NAM, MACT) VALUES ('HP009', 3, 2024, 'CQ');
+INSERT INTO N09_KHMO (MAHP, HK, NAM, MACT) VALUES ('HP010', 1, 2024, 'CLC');
+/
+
+----------------------------------------------------------------
+-- Chèn dữ liệu vào bảng N09_PHANCONG
+----------------------------------------------------------------
+INSERT INTO N09_PHANCONG (MAGV, MAHP, HK, NAM, MACT) VALUES ('NV201', 'HP001', 1, 2024, 'CQ');
+INSERT INTO N09_PHANCONG (MAGV, MAHP, HK, NAM, MACT) VALUES ('NV202', 'HP002', 2, 2024, 'CLC');
+INSERT INTO N09_PHANCONG (MAGV, MAHP, HK, NAM, MACT) VALUES ('NV203', 'HP003', 3, 2024, 'CTTT');
+INSERT INTO N09_PHANCONG (MAGV, MAHP, HK, NAM, MACT) VALUES ('NV204', 'HP004', 1, 2024, 'VP');
+INSERT INTO N09_PHANCONG (MAGV, MAHP, HK, NAM, MACT) VALUES ('NV205', 'HP005', 2, 2024, 'CQ');
+INSERT INTO N09_PHANCONG (MAGV, MAHP, HK, NAM, MACT) VALUES ('NV206', 'HP006', 3, 2024, 'CLC');
+INSERT INTO N09_PHANCONG (MAGV, MAHP, HK, NAM, MACT) VALUES ('NV207', 'HP007', 1, 2024, 'CTTT');
+INSERT INTO N09_PHANCONG (MAGV, MAHP, HK, NAM, MACT) VALUES ('NV208', 'HP008', 2, 2024, 'VP');
+INSERT INTO N09_PHANCONG (MAGV, MAHP, HK, NAM, MACT) VALUES ('NV209', 'HP009', 3, 2024, 'CQ');
+INSERT INTO N09_PHANCONG (MAGV, MAHP, HK, NAM, MACT) VALUES ('NV210', 'HP010', 1, 2024, 'CLC');
+/
+
+----------------------------------------------------------------
+-- Chèn dữ liệu vào bảng N09_DANGKY
+----------------------------------------------------------------
+INSERT INTO N09_DANGKY (MASV, MAGV, MAHP, HK, NAM, MACT, DIEMTH, DIEMQT, DIEMCK, DIEMTK) VALUES ('SV001', 'NV201', 'HP001', 1, 2024, 'CQ', 8.5, 7.0, 9.0, 8.0);
+INSERT INTO N09_DANGKY (MASV, MAGV, MAHP, HK, NAM, MACT, DIEMTH, DIEMQT, DIEMCK, DIEMTK) VALUES ('SV002', 'NV202', 'HP002', 2, 2024, 'CLC', 7.8, 7.5, 8.0, 7.8);
+INSERT INTO N09_DANGKY (MASV, MAGV, MAHP, HK, NAM, MACT, DIEMTH, DIEMQT, DIEMCK, DIEMTK) VALUES ('SV003', 'NV203', 'HP003', 3, 2024, 'CTTT', 9.0, 8.5, 9.5, 9.0);
+INSERT INTO N09_DANGKY (MASV, MAGV, MAHP, HK, NAM, MACT, DIEMTH, DIEMQT, DIEMCK, DIEMTK) VALUES ('SV004', 'NV204', 'HP004', 1, 2024, 'VP', 6.5, 7.0, 7.0, 6.8);
+INSERT INTO N09_DANGKY (MASV, MAGV, MAHP, HK, NAM, MACT, DIEMTH, DIEMQT, DIEMCK, DIEMTK) VALUES ('SV005', 'NV205', 'HP005', 2, 2024, 'CQ', 7.2, 7.0, 7.5, 7.2);
+INSERT INTO N09_DANGKY (MASV, MAGV, MAHP, HK, NAM, MACT, DIEMTH, DIEMQT, DIEMCK, DIEMTK) VALUES ('SV006', 'NV206', 'HP006', 3, 2024, 'CLC', 8.0, 7.5, 8.5, 8.0);
+INSERT INTO N09_DANGKY (MASV, MAGV, MAHP, HK, NAM, MACT, DIEMTH, DIEMQT, DIEMCK, DIEMTK) VALUES ('SV007', 'NV207', 'HP007', 1, 2024, 'CTTT', 8.4, 7.5, 8.5, 8.1);
+INSERT INTO N09_DANGKY (MASV, MAGV, MAHP, HK, NAM, MACT, DIEMTH, DIEMQT, DIEMCK, DIEMTK) VALUES ('SV008', 'NV208', 'HP008', 2, 2024, 'VP', 6.9, 7.0, 7.5, 7.1);
+INSERT INTO N09_DANGKY (MASV, MAGV, MAHP, HK, NAM, MACT, DIEMTH, DIEMQT, DIEMCK, DIEMTK) VALUES ('SV009', 'NV209', 'HP009', 3, 2024, 'CQ', 7.5, 7.0, 7.8, 7.4);
+INSERT INTO N09_DANGKY (MASV, MAGV, MAHP, HK, NAM, MACT, DIEMTH, DIEMQT, DIEMCK, DIEMTK) VALUES ('SV010', 'NV210', 'HP010', 1, 2024, 'CLC', 8.1, 7.5, 8.5, 8.0);
+/
+
+
---
+++ CQATBM-09-PH2-Script/N09_PH2_USP_UV_FUNC.sql
@@ -0,0 +1,348 @@
+-- Lớp: CQ2021/1
+-- Nhóm: 09
+-- Thành viên:
+-- 21120037 - Mã Thùy Anh
+-- 21120060 - Nguyễn Long Giang
+-- 21120082 - Phan Quốc Huy 
+-- 21120117 - Lê Thị Hồng Phượng
+
+
+
+----------------------------------------------------------------
+-- Script tạo View cho tất cả Table trong Database
+----------------------------------------------------------------
+CREATE OR REPLACE VIEW UV_N09_NHANSU
+AS
+    SELECT * FROM N09_NHANSU;
+    
+CREATE OR REPLACE VIEW UV_N09_SINHVIEN
+AS
+    SELECT * FROM N09_SINHVIEN;
+    
+CREATE OR REPLACE VIEW UV_N09_DONVI
+AS
+    SELECT * FROM N09_DONVI;
+    
+CREATE OR REPLACE VIEW UV_N09_HOCPHAN
+AS
+    SELECT * FROM N09_HOCPHAN;
+    
+CREATE OR REPLACE VIEW UV_N09_KHMO
+AS
+    SELECT * FROM N09_KHMO;
+        
+CREATE OR REPLACE VIEW UV_N09_PHANCONG
+AS
+    SELECT * FROM N09_PHANCONG;    
+    
+CREATE OR REPLACE VIEW UV_N09_DANGKY
+AS
+    SELECT * FROM N09_DANGKY;    
+/
+
+
+----------------------------------------------------------------
+-- Stored Procedure SELECT bất kỳ Table nào
+-- Tham số truyền vào: Tên Table
+-- Tham số optional: Điều kiện WHERE
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_SELECT_ANY_TABLE(
+    OUTPUT OUT SYS_REFCURSOR,
+    STR_TABLE_NAME IN VARCHAR2,
+    STR_CONDITION IN VARCHAR2 DEFAULT NULL)
+AS
+BEGIN
+    IF STR_CONDITION IS NULL THEN
+        OPEN OUTPUT FOR 'SELECT * FROM ' || STR_TABLE_NAME;
+    ELSE
+        OPEN OUTPUT FOR 'SELECT * FROM ' || STR_TABLE_NAME || ' WHERE ' || STR_CONDITION;
+    END IF;
+END;
+/
+
+-- -- Test
+-- -- SELECT tất cả các dòng của Table N09_NHANVIEN
+-- VARIABLE rc REFCURSOR;
+--     EXECUTE SYS.N09_SELECT_ANY_TABLE(:rc, 'N09_NHANVIEN');
+-- PRINT rc;
+
+-- -- Không SELECT được dòng nào trong Table N09_NHANVIEN
+-- VARIABLE rc REFCURSOR;
+--     EXECUTE SYS.N09_SELECT_ANY_TABLE(:rc, 'N09_NHANVIEN', '1=0');
+-- PRINT rc;
+
+
+
+----------------------------------------------------------------
+-- Stored Procedure SELECT tất cả các Roles
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_SELECT_DBA_ROLES(
+    OUTPUT OUT SYS_REFCURSOR)
+AS
+BEGIN
+    OPEN OUTPUT FOR SELECT ROLE, ROLE_ID FROM SYS.DBA_ROLES;
+END;
+/
+
+-- -- Test
+-- VARIABLE rc REFCURSOR;
+-- EXECUTE N09_SELECT_DBA_ROLES(:rc);
+-- PRINT rc;
+
+
+
+----------------------------------------------------------------
+-- Stored Procedure SELECT tất cả các Privillege của Roles
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_SELECT_DBA_ROLE_PRIVS(
+    OUTPUT OUT SYS_REFCURSOR)
+AS
+BEGIN
+    OPEN OUTPUT FOR SELECT * FROM SYS.DBA_ROLE_PRIVS;
+END;
+/
+
+-- -- Test
+-- VARIABLE rc REFCURSOR;
+-- EXECUTE N09_SELECT_DBA_ROLE_PRIVS(:rc);
+-- PRINT rc;
+
+
+
+----------------------------------------------------------------
+-- Stored Procedure SELECT tất cả các Users
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_SELECT_ALL_USERS(
+    OUTPUT OUT SYS_REFCURSOR)
+AS
+BEGIN
+    OPEN OUTPUT FOR SELECT * FROM SYS.ALL_USERS;
+END;
+/
+
+
+
+----------------------------------------------------------------
+-- Stored Procedure SELECT tất cả các Privillege của Roles tính đến cột
+-- Tham số truyền vào: Tên User hoặc Role
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_SELECT_USER_OR_ROLE_PRIVS(
+    OUTPUT OUT SYS_REFCURSOR,
+    USER_OR_ROLE_NAME IN VARCHAR2)
+AS
+BEGIN
+    OPEN OUTPUT FOR SELECT GRANTOR, GRANTEE, TABLE_SCHEMA, TABLE_NAME,
+                        NULL AS COLUMN_NAME, PRIVILEGE, GRANTABLE, HIERARCHY, COMMON, TYPE, INHERITED 
+                    FROM ALL_TAB_PRIVS 
+                    WHERE GRANTEE = USER_OR_ROLE_NAME 
+                    UNION ALL
+                    SELECT GRANTOR, GRANTEE, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, PRIVILEGE, GRANTABLE,
+                        NULL AS HIERARCHY, COMMON, NULL AS TYPE, INHERITED 
+                    FROM ALL_COL_PRIVS
+                    WHERE GRANTEE =  USER_OR_ROLE_NAME;
+END;
+/
+
+-- -- Test
+-- VARIABLE rc REFCURSOR;
+-- EXECUTE N09_SELECT_USER_OR_ROLE_PRIVS(:rc, 'HUYP');
+-- PRINT rc;
+
+
+
+----------------------------------------------------------------
+-- Stored Procedure SELECT tất cả các Privillege của Roles tính đến cột (bản rút gọn)
+-- Tham số truyền vào: Tên User hoặc Role
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_SELECT_USER_OR_ROLE_PRIVS_SIMPLIFY(
+    OUTPUT OUT SYS_REFCURSOR,
+    USER_OR_ROLE_NAME IN VARCHAR2)
+AS
+BEGIN
+    OPEN OUTPUT FOR SELECT TABLE_NAME, NULL AS COLUMN_NAME, PRIVILEGE, GRANTABLE
+                    FROM SYS.ALL_TAB_PRIVS 
+                    WHERE GRANTEE = USER_OR_ROLE_NAME 
+                    UNION ALL
+                    SELECT TABLE_NAME, COLUMN_NAME, PRIVILEGE, GRANTABLE
+                    FROM SYS.ALL_COL_PRIVS 
+                    WHERE GRANTEE = USER_OR_ROLE_NAME;
+END;
+/
+
+-- -- Test
+-- VARIABLE rc REFCURSOR;
+-- EXECUTE N09_SELECT_USER_OR_ROLE_PRIVS_SIMPLIFY(:rc, 'KH1');
+-- PRINT rc;
+
+
+
+----------------------------------------------------------------
+-- Stored Procedure SELECT tất cả các Tables
+-- Tham số truyền vào: Chuỗi con của tên Table
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_SELECT_DBA_TABLES(
+    OUTPUT OUT SYS_REFCURSOR,
+    SUBSTR IN VARCHAR2)
+AS
+BEGIN
+    OPEN OUTPUT FOR SELECT TABLE_NAME FROM DBA_TABLES WHERE TABLE_NAME LIKE '%' || SUBSTR || '%';
+END;
+/
+
+VARIABLE rc REFCURSOR;
+    EXECUTE SYS.N09_SELECT_DBA_TABLES(:rc, 'N09_');
+PRINT rc;
+
+
+
+----------------------------------------------------------------
+-- Stored Procedure SELECT tất cả các cột của Table
+-- Tham số truyền vào: Tên Table
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_SELECT_TAB_COLUMNS(
+    OUTPUT OUT SYS_REFCURSOR,
+    STR_TABLE_NAME IN VARCHAR2)
+AS
+BEGIN
+    OPEN OUTPUT FOR SELECT COLUMN_NAME FROM USER_TAB_COLUMNS WHERE TABLE_NAME = STR_TABLE_NAME;
+END;
+/
+
+VARIABLE rc REFCURSOR;
+    EXECUTE SYS.N09_SELECT_TAB_COLUMNS(:rc, 'N09_NHANVIEN');
+PRINT rc;
+
+
+
+----------------------------------------------------------------
+-- Stored Procedure tạo User
+-- Tham số truyền vào: Tên User, Mật khẩu
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_CREATE_USER(
+    USRNAME IN VARCHAR2,
+    PASSWRD IN VARCHAR2)
+IS
+BEGIN
+    IF LENGTH(USRNAME) = 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Invalid username.');
+    END IF;
+
+    EXECUTE IMMEDIATE 'ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE';
+    EXECUTE IMMEDIATE 'CREATE USER ' || USRNAME || ' IDENTIFIED BY ' || PASSWRD;
+    EXECUTE IMMEDIATE 'GRANT CONNECT TO "' || USRNAME || '"';
+END;
+/
+
+
+
+----------------------------------------------------------------
+-- Stored Procedure tạo Role
+-- Tham số truyền vào: Tên Role
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_CREATE_ROLE(
+    ROLENAME IN VARCHAR2)
+IS
+BEGIN
+    IF LENGTH(ROLENAME) = 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Invalid role name.');
+    END IF;
+
+    EXECUTE IMMEDIATE 'ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE';
+    EXECUTE IMMEDIATE 'CREATE ROLE ' || ROLENAME;
+END;
+/
+
+
+
+----------------------------------------------------------------
+-- Stored Procedure xóa User
+-- Tham số truyền vào: Tên User
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_DROP_USER(
+    USERNAME IN VARCHAR2)
+IS
+BEGIN
+    EXECUTE IMMEDIATE 'ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE';
+    EXECUTE IMMEDIATE 'DROP USER ' || USERNAME;
+END;
+/
+
+
+----------------------------------------------------------------
+-- Stored Procedure xóa Role
+-- Tham số truyền vào: Tên Role
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_DROP_ROLE(
+    ROLENAME IN VARCHAR2)
+IS
+BEGIN
+    EXECUTE IMMEDIATE 'ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE';
+    EXECUTE IMMEDIATE 'DROP ROLE ' || ROLENAME;
+END;
+/
+
+
+----------------------------------------------------------------
+-- Stored Procedure gán quyền cho User hoặc Role
+-- Tham số truyền vào: Tên quyền, Tên User hoặc Role
+-- Tham số optional: Quyền WITH GRANT OPTION, Tên Table hoặc View, Tên cột
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_GRANT_PRIVILEGE
+(PRIVILEGE IN VARCHAR2, USER_OR_ROLE IN VARCHAR2, WITH_GRANT_OPTION IN VARCHAR2 DEFAULT NULL,
+TABLE_OR_VIEW IN VARCHAR2 DEFAULT NULL, COLUMNS_CSV IN VARCHAR2 DEFAULT NULL)
+AS
+    STRCOL VARCHAR2(100); 
+BEGIN
+    IF TABLE_OR_VIEW IS NULL THEN
+        EXECUTE IMMEDIATE 'GRANT ' || PRIVILEGE || ' TO ' || USER_OR_ROLE || WITH_GRANT_OPTION;
+    ELSE
+        IF COLUMNS_CSV IS NOT NULL THEN
+            STRCOL := ' (' || COLUMNS_CSV || ')';
+        END IF;
+        EXECUTE IMMEDIATE 'GRANT ' || PRIVILEGE || STRCOL || ' ON ' || TABLE_OR_VIEW || ' TO ' || USER_OR_ROLE || WITH_GRANT_OPTION;
+    END IF;
+END;
+/
+
+
+----------------------------------------------------------------
+-- Stored Procedure thu hồi quyền từ User hoặc Role
+-- Tham số truyền vào: Tên quyền, Tên User hoặc Role
+-- Tham số optional: Tên Table hoặc View, Tên cột
+----------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE N09_REVOKE_PRIVILEGE
+(PRIVILEGE IN VARCHAR2, USER_OR_ROLE IN VARCHAR2, 
+TABLE_OR_VIEW IN VARCHAR2 DEFAULT NULL,
+COLUMNS_CSV IN VARCHAR2 DEFAULT NULL)
+AS
+    STRCOL VARCHAR2(100); 
+BEGIN
+    IF TABLE_OR_VIEW IS NULL THEN
+        EXECUTE IMMEDIATE 'REVOKE ' || PRIVILEGE || ' FROM ' || USER_OR_ROLE;
+    ELSE
+        IF COLUMNS_CSV IS NOT NULL THEN
+            STRCOL := ' (' || COLUMNS_CSV || ')';
+        END IF;
+        EXECUTE IMMEDIATE 'REVOKE ' || PRIVILEGE || STRCOL || ' ON ' || TABLE_OR_VIEW || ' FROM ' || USER_OR_ROLE;
+    END IF;
+END;
+/
+
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
