d5289f7fa9730bf8b69a3c724fbc0d802078167a
Giangfs <123853667+Giangfs@users.noreply.github.com>
2024-06-01 18:03:10 +0700

    Done UI Homepage Staff

--- oracle-database-administator/Staff/HomePageStaff.xaml.cs
+++ oracle-database-administator/Staff/HomePageStaff.xaml.cs
@@ -24,5 +24,54 @@ namespace oracle_database_administator.Staff
         {
             InitializeComponent();
         }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow is MainWindow mainWindow && mainWindow.MainFrame != null)
+            {
+                mainWindow.MainFrame.Navigate(new oracle_database_administator.Dashboard());
+            }
+        }
+
+        private void TTCN_Click(object sender, RoutedEventArgs e)
+        {
+            Label_TTCN.Visibility = Visibility.Visible;
+            DataGrid_TTCN.Visibility = Visibility.Visible;
+        }
+
+        private void TTDonVi_Click(object sender, RoutedEventArgs e)
+        {
+            Label_DonVi.Visibility = Visibility.Visible;
+            Table_DonVi.Visibility = Visibility.Visible;
+        }
+
+        private void KHHocTap_Click(object sender, RoutedEventArgs e)
+        {
+            Label_KHHT.Visibility = Visibility.Visible;
+            TextBlock_DSHocPhan.Visibility = Visibility.Visible;
+            TextBlock_KeHoachMo.Visibility = Visibility.Visible;    
+        }
+
+        private void DSHocPhan_Click(object sender, RoutedEventArgs e)
+        {
+            Table_DsHocPhan.Visibility = Visibility.Visible;
+        }
+
+        private void KeHoachMo_Click(object sender, RoutedEventArgs e)
+        {
+            Table_KeHoachMo.Visibility = Visibility.Visible;
+        }
+
+        private void DSSinhVien_Click(object sender, RoutedEventArgs e)
+        {
+            Label_DsSinhVien.Visibility = Visibility.Visible;
+            Table_DsSinhVien.Visibility = Visibility.Visible;
+        }
+
+        private void EditSDT_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlock_SDT.IsHitTestVisible = true;
+            TextBlock_SDT.Focusable = true;
+        }
     }
 }
