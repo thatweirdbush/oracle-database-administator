591e097901aec925a77dc2fdf7288351b2469c3e
Giangfs <123853667+Giangfs@users.noreply.github.com>
2024-06-02 18:55:14 +0700

    fix display UI Homepage Staff

--- oracle-database-administator/Staff/HomePageStaff.xaml.cs
+++ oracle-database-administator/Staff/HomePageStaff.xaml.cs
@@ -24,6 +24,20 @@ namespace oracle_database_administator.Staff
         {
             InitializeComponent();
         }
+        private void HideAllElements()
+        {
+            Label_TTCN.Visibility = Visibility.Hidden;
+            DataGrid_TTCN.Visibility = Visibility.Hidden;
+            Label_DonVi.Visibility = Visibility.Hidden;
+            Table_DonVi.Visibility = Visibility.Hidden;
+            Label_KHHT.Visibility = Visibility.Hidden;
+            TextBlock_DSHocPhan.Visibility = Visibility.Hidden;
+            TextBlock_KeHoachMo.Visibility = Visibility.Hidden;
+            Table_DsHocPhan.Visibility = Visibility.Hidden;
+            Table_KeHoachMo.Visibility = Visibility.Hidden;
+            Label_DsSinhVien.Visibility = Visibility.Hidden;
+            Table_DsSinhVien.Visibility = Visibility.Hidden;
+        }
 
         private void HomeButton_Click(object sender, RoutedEventArgs e)
         {
@@ -35,18 +49,21 @@ namespace oracle_database_administator.Staff
 
         private void TTCN_Click(object sender, RoutedEventArgs e)
         {
+            HideAllElements();
             Label_TTCN.Visibility = Visibility.Visible;
             DataGrid_TTCN.Visibility = Visibility.Visible;
         }
 
         private void TTDonVi_Click(object sender, RoutedEventArgs e)
         {
+            HideAllElements();
             Label_DonVi.Visibility = Visibility.Visible;
             Table_DonVi.Visibility = Visibility.Visible;
         }
 
         private void KHHocTap_Click(object sender, RoutedEventArgs e)
         {
+            HideAllElements();
             Label_KHHT.Visibility = Visibility.Visible;
             TextBlock_DSHocPhan.Visibility = Visibility.Visible;
             TextBlock_KeHoachMo.Visibility = Visibility.Visible;    
@@ -54,16 +71,25 @@ namespace oracle_database_administator.Staff
 
         private void DSHocPhan_Click(object sender, RoutedEventArgs e)
         {
+            HideAllElements();
+            Label_KHHT.Visibility = Visibility.Visible;
+            TextBlock_DSHocPhan.Visibility = Visibility.Visible;
+            TextBlock_KeHoachMo.Visibility = Visibility.Visible;
             Table_DsHocPhan.Visibility = Visibility.Visible;
         }
 
         private void KeHoachMo_Click(object sender, RoutedEventArgs e)
         {
+            HideAllElements();
+            Label_KHHT.Visibility = Visibility.Visible;
+            TextBlock_DSHocPhan.Visibility = Visibility.Visible;
+            TextBlock_KeHoachMo.Visibility = Visibility.Visible;
             Table_KeHoachMo.Visibility = Visibility.Visible;
         }
 
         private void DSSinhVien_Click(object sender, RoutedEventArgs e)
         {
+            HideAllElements();
             Label_DsSinhVien.Visibility = Visibility.Visible;
             Table_DsSinhVien.Visibility = Visibility.Visible;
         }
